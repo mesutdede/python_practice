af3a75bfd732a6c2ac69de2aa65bc4fd2f7e9d24
Mesut Dede <mesutdede@gmail.com>
2020-11-14 22:04:08 +0300

    working with conditions

---
+++ Basic/conditionals.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+number1 = 10
+number2 = 20
+
+if number1 > number2:
+    print("number1 bigger than number2")
+elif number1 == number2:
+    print("number1 equal number2")
+else:
+    print("number1 smaller than number2")
