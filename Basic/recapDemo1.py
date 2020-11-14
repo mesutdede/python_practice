da22138c34908c1fb3680ef8f56839254d2f28c5
Mesut Dede <mesutdede@gmail.com>
2020-11-14 22:05:05 +0300

    recap demo 1

---
+++ Basic/recapDemo1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+lights = ["Red","Yellow","Green"]
+
+current = lights[2]
+
+print(current)
+
+if current == "Red":
+    print("STOP!")
+elif current == "Yellow":
+    print("READY!")
+else:
+    print("GO!")
